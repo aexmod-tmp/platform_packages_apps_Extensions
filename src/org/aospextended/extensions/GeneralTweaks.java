bd713a87aae42c15f02b785cbdbbdae587ce9a6a
DennySPB <dennyspb@gmail.com>
2018-01-17 00:09:45 +0300

    Remove System UI style preferences

--- src/org/aospextended/extensions/GeneralTweaks.java
+++ src/org/aospextended/extensions/GeneralTweaks.java
@@ -54,10 +54,6 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
     private PreferenceCategory mLedsCategory;
     private Preference mChargingLeds;
 
-    private static final String SYSTEMUI_THEME_STYLE = "systemui_theme_style";
-
-    private ListPreference mSystemUIThemeStyle;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,14 +62,6 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
-
-	mSystemUIThemeStyle = (ListPreference) findPreference(SYSTEMUI_THEME_STYLE);
-        int systemUIThemeStyle = Settings.System.getInt(resolver,
-                Settings.System.SYSTEM_UI_THEME, 0);
-        int valueIndex = mSystemUIThemeStyle.findIndexOfValue(String.valueOf(systemUIThemeStyle));
-        mSystemUIThemeStyle.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
-        mSystemUIThemeStyle.setSummary(mSystemUIThemeStyle.getEntry());
-        mSystemUIThemeStyle.setOnPreferenceChangeListener(this);
 
         mLedsCategory = (PreferenceCategory) findPreference("light_category");
         mChargingLeds = (Preference) findPreference("battery_charging_light");
@@ -105,14 +93,6 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
 	 ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mSystemUIThemeStyle) {
-            String value = (String) newValue;
-            Settings.System.putInt(resolver,
-                    Settings.System.SYSTEM_UI_THEME, Integer.valueOf(value));
-            int valueIndex = mSystemUIThemeStyle.findIndexOfValue(value);
-            mSystemUIThemeStyle.setSummary(mSystemUIThemeStyle.getEntries()[valueIndex]);
-            return true;
-        }
         return false;
     }
 }
