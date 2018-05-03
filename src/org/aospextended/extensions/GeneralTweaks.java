062942ad614791e349a4cb1e88b72cfa2a27e4aa
Alexey Derendyaev <alexxxdev@yandex.ru>
2018-05-03 10:33:20 +0700

    Added features flash-on-waiting [3/3]

--- src/org/aospextended/extensions/GeneralTweaks.java
+++ src/org/aospextended/extensions/GeneralTweaks.java
@@ -16,47 +16,35 @@
 
 package org.aospextended.extensions;
 
-import android.app.ActivityManagerNative;
-import android.content.Context;
 import android.content.ContentResolver;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.WindowManagerGlobal;
-import android.view.IWindowManager;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.Utils;
 
+import org.aospextended.extensions.preference.CustomSeekBarPreference;
+
 public class GeneralTweaks extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
     private static final String SCREEN_OFF_ANIMATION = "screen_off_animation";
     private static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+    private static final String FLASH_ON_CALL_WAITING_DELAY = "flash_on_call_waiting_delay";
 
     private PreferenceCategory mLedsCategory;
     private Preference mChargingLeds;
     private ListPreference mScreenOffAnimation;
     private ListPreference mMSOB;
+
+    private CustomSeekBarPreference mFlashOnCallWaitingDelay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -97,6 +85,10 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
         mMSOB.setValue(String.valueOf(mMSOBValue));
         mMSOB.setSummary(mMSOB.getEntry());
         mMSOB.setOnPreferenceChangeListener(this);
+
+        mFlashOnCallWaitingDelay = (CustomSeekBarPreference) findPreference(FLASH_ON_CALL_WAITING_DELAY);
+        mFlashOnCallWaitingDelay.setValue(Settings.System.getInt(resolver, Settings.System.FLASH_ON_CALLWAITING_DELAY, 200));
+        mFlashOnCallWaitingDelay.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -126,7 +118,11 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
             mMSOB.setValue(String.valueOf(value));
             mMSOB.setSummary(mMSOB.getEntries()[value]);
             return true; 
-	}
+	} else if (preference == mFlashOnCallWaitingDelay) {
+            int val = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(), Settings.System.FLASH_ON_CALLWAITING_DELAY, val);
+            return true;
+        }
 	    return false;
     }
 }
